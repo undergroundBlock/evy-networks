f29480c1b6fb6cda86dfa747ad0415eab9ebc638
undergroundBlock <107368307+undergroundBlock@users.noreply.github.com>
2025-10-26 01:58:14 +0300

    Updates

---
+++ _next/static/7XFiPfcP2aIg59BSWQ_5M/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/evy-networks//_next/:path+",
+        "destination": "/evy-networks/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
