b293034cf9b7366f9b20631e2f819a5e943d911e
undergroundBlock <107368307+undergroundBlock@users.noreply.github.com>
2025-10-26 01:17:29 +0300

    Initial commit

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export', // enables static export
+  basePath: '/evy-networks', // 👈 replace with your GitHub repo name
+  images: {
+    unoptimized: true, // GitHub Pages doesn’t support Next Image Optimization
+  },
+  assetPrefix: '/evy-networks/', // ensures static assets load correctly
 };
 
 export default nextConfig;
